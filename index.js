4f3b26c1f73b887a64b3eeef44db5a9f3a453780
Aqsa729 <102195351+Aqsa729@users.noreply.github.com>
2022-04-11 14:05:39 -0400

    commit tree f25e67c9258349a7594620ae04060b6a39bf840a

---
+++ index.js
@@ -0,0 +1,5 @@
+1) shout(string)
+receives one argument and returns it in all caps:
+ReferenceError: shout is not defined
+at Context.<anonymous> (test/index-test.js:4:5)
+at processImmediate (internal/timers.js:461:21)
